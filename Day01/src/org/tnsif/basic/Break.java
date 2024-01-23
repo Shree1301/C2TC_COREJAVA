de655194327d7282c76c7a4ff9478fc2f26c10c8
shree gupta <shree gupta@192.168.137.115>
2024-01-23 16:16:54 +0530

    first commit

---
+++ Day01/src/org/tnsif/basic/Break.java
@@ -0,0 +1,17 @@
+package org.tnsif.basic;
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=3;i<=50;i+=3)
+		{
+			if(i==39)
+			{
+				break;
+			}
+			System.out.println(i+" ");		}
+
+	}
+
+}
